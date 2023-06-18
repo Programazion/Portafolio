cc60a4fea733f2a35a183f9342b1f18c6e74878e
Daniel Alejandro Sanchez Paez <programazion1@gmail.com>
2023-06-18 17:12:45 -0500

    First Commit

---
+++ eventoInicio.js
@@ -0,0 +1,6 @@
+function myAction4(onclick) {
+    alert("Estamos muy felices que nos hayas encontrado en la web. Somos una multinacional dedicada al desarrollo de software y haremos nuestro mayor esfuerzo para cumplir tus expectativas. Bienvenid@s");
+    var data= document.getElementById('inicio');
+    data.style.backgroundColor= "red";
+
+}
