fbf7fc67303005d36730de5b04ee9fed2d710b1f
Daniel Alejandro Sanchez Paez <programazion1@gmail.com>
2023-06-19 13:35:45 -0500

    Botón inicio-index

--- eventoInicio.js
+++ eventoInicio.js
@@ -1,5 +1,5 @@
 function myAction4(onclick) {
     alert("Estamos muy felices que nos hayas encontrado en la web. Somos una multinacional dedicada al desarrollo de software y haremos nuestro mayor esfuerzo para cumplir tus expectativas. Bienvenid@s");
     var data= document.getElementById('inicio');
-    data.style.backgroundColor= "red";
+    data.style.backgroundColor= "pink";
 }
