d60712e79cd50497ed0c93b030495125e3ebb21d
Daniel Alejandro Sanchez Paez <programazion1@gmail.com>
2023-06-20 10:58:43 -0500

    Using a Framework

---
+++ starter-template/eventos.js
@@ -0,0 +1,31 @@
+console.log('Cargando JavaScript');
+console.log("Loading JavaScript Events");
+
+
+function myAction(onclick) {
+    alert("Bienvenid@s a Programazion Software company");
+    var data= document.getElementById('home');
+    data.style.backgroundColor= "red";
+
+}
+
+function myAction2(onclick) {
+    alert("Acá podrás conocer un poco más sobre mi perfil");
+    var data= document.getElementById('cv');
+    data.style.backgroundColor= "yellow";
+
+}
+
+function myAction3(onclick) {
+    alert(" Bienvenid@s al Home");
+    var data= document.getElementById('home');
+    data.style.backgroundColor= "orange";
+
+}
+
+function myAction4(onclick) {
+    alert(" Bienvenid@s al Home");
+    var data= document.getElementById('home');
+    data.style.backgroundColor= "orange";
+
+}
